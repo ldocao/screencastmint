0149a1bf8ebc3aeafcf656417d9b92f574b7b61b
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-09-13 18:31:22 +0200

    docs(github) : add commit method documentation
    fixes #7

--- app/models/githuber.rb
+++ app/models/githuber.rb
@@ -1,4 +1,7 @@
 class Githuber < ActiveRecord::Base
+
+  #
+  # Get all commits from screencastmint website project
   def self.commits
     self.client.repos.commits.all("screencastmint", "screencastmint")
   end
